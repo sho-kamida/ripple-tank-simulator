d1d1ed427c5f69645cd5abbb480b8b52c33fc0bc
tanahari <sho.k3141592@icloud.com>
2026-07-12 23:32:16 +0900

    chore: add temporary debug loop for WasmWave1D verfication

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,15 +1,27 @@
-import './style.css'
-import init, { add } from './pkg';
+import init, { WasmWave1D } from './pkg'
 
-// Wasmのロードと初期化は非同期で行われるため、トップレベルで実行します
-async function runWasm() {
-  // Wasmモジュールを初期化
-  await init()
-  console.log("Wasmが正常に読み込まれました！");
+async function run() {
+  await init();
+  const wave = new WasmWave1D(100); // 100要素の波を作成
+  let isRunning = true;
 
-  // 試しに add 関数を実行
-  const result = add(10, 32);
-  console.log(`Rust側の計算結果: 10 + 32 = ${result}`);
+  // 数値表示用の要素を取得
+  const debugEl = document.getElementById('debug') as HTMLPreElement
+
+  function loop() {
+    if (isRunning) {
+      wave.pluck(5, -1.0);
+      isRunning = false
+    }
+    wave.tick();
+
+    const data = wave.get_data();
+
+    // 配列の表示
+    debugEl.textContent = Array.from(data).map(n => n.toFixed(2).padStart(7)).join(" ")
+
+    setTimeout(loop, 1000)
+  }
+  loop();
 }
-
-runWasm();
+run();
